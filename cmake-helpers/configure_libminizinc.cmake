fbd732183efbe01876f6867693a8ea8b66afb059
Kevin Leo <kevin.kevinleo@gmail.com>
2018-07-06 15:57:53 +1000

    Added libminizinc and cmake helpers

---
+++ cmake-helpers/configure_libminizinc.cmake
@@ -0,0 +1,22 @@
+if(USE_SYSTEM_LIBMINIZINC)
+  find_package(libminizinc)
+endif()
+
+if(NOT libminizinc_FOUND)
+  message("cmake-helpers/configure_libminizinc.cmake: Not using system libminizinc (set USE_SYSTEM_LIBMINIZINC). Submodule will be compiled instead.")
+  set(LIBMINIZINC_SOURCE_DIR "${CMAKE_SOURCE_DIR}/submodules/libminizinc_develop/")
+  add_subdirectory(${LIBMINIZINC_SOURCE_DIR} EXCLUDE_FROM_ALL)
+  set(libminizinc_INCLUDE_DIRS 
+    ${CMAKE_BINARY_DIR}/submodules/libminizinc_develop/include
+    ${LIBMINIZINC_SOURCE_DIR}/include)
+  find_program(BISON_EXEC bison)
+  if(NOT BISON_EXEC)
+    set(libminizinc_INCLUDE_DIRS ${libminizinc_INCLUDE_DIRS} ${LIBMINIZINC_SOURCE_DIR}/lib/cached/)
+  endif()
+  set(libminizinc_FOUND true)
+else()
+  message("System MiniZinc library found at ${libminizinc_INCLUDE_DIRS}!")
+endif()
+
+message("cmake-helpers/configure_libminizinc.cmake: Using includes: ${libminizinc_INCLUDE_DIRS}")
+include_directories(${libminizinc_INCLUDE_DIRS})
