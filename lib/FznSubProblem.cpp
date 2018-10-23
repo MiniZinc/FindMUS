efe01f4598f261991fd249ef703d9d39dd47fa59
Guido Tack <guido.tack@monash.edu>
2018-10-24 09:58:34 +1100

    Fix call to MiniZinc::parse

--- lib/FznSubProblem.cpp
+++ lib/FznSubProblem.cpp
@@ -102,7 +102,7 @@ namespace HierMUS {
 
     includes.push_back(mo.mzn_stdlib_dir + "/std/");
 
-    fzn_model = MiniZinc::parse(fzn_env, {fznpath}, {}, includes, false, false, false, std::cerr);
+    fzn_model = MiniZinc::parse(fzn_env, {fznpath}, {}, "", "", includes, false, false, false, std::cerr);
     MiniZinc::SolveI* si = fzn_model->solveItem();
     si->st(MiniZinc::SolveI::ST_SAT);
     si->e(NULL);
