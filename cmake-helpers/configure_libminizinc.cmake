4e977af1c3ab4dd3cff40f13236ec960aca89583
Kevin Leo <kevin.kevinleo@gmail.com>
2020-01-09 16:37:10 +1100

    Removed libminizinc submodule

--- cmake-helpers/configure_libminizinc.cmake
+++ cmake-helpers/configure_libminizinc.cmake
@@ -1,18 +1,7 @@
-if(USE_SYSTEM_LIBMINIZINC)
-  find_package(libminizinc)
-endif()
+find_package(libminizinc)
 
 if(NOT libminizinc_FOUND)
-  message("cmake-helpers/configure_libminizinc.cmake: Not using system libminizinc (set USE_SYSTEM_LIBMINIZINC). Submodule will be compiled instead.")
-  set(LIBMINIZINC_SOURCE_DIR "${CMAKE_SOURCE_DIR}/submodules/libminizinc_develop/")
-  add_subdirectory(${LIBMINIZINC_SOURCE_DIR} EXCLUDE_FROM_ALL)
-  set(libminizinc_INCLUDE_DIRS 
-    ${CMAKE_BINARY_DIR}/submodules/libminizinc_develop/include
-    ${LIBMINIZINC_SOURCE_DIR}/include
-    ${LIBMINIZINC_SOURCE_DIR}/lib/cached/) # Include the cached directory last
-  set(libminizinc_FOUND true)
-else()
-  message("System MiniZinc library found at ${libminizinc_INCLUDE_DIRS}!")
+  error("cmake-helpers/configure_libminizinc.cmake: Cannot find system libminizinc needed for FindMUS.")
 endif()
 
 message("cmake-helpers/configure_libminizinc.cmake: Using includes: ${libminizinc_INCLUDE_DIRS}")
